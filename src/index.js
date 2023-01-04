ffaf5789ed0d73fdc78ddcdca19be73ffb0f9f5a
Viktoriia181818 <vcibulskaa4@gmail.com>
2023-01-04 15:30:46 +0200

    push

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import './css/styles.css';
 import { fetchCountries } from './js/fetchcountries';
-import { countryСardTeemplate, countryListTemplate } from './js/template';
+import { countryCardTeemplate, countryListTemplate } from './js/template';
 import { refs } from './js/refs';
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
 import debounce from 'lodash.debounce';
